2ad1caa5f5d79b4c6629dae13d3520ffa33078fe
Chris Harrison <unsliced@gmail.com>
2023-02-19 11:32:16 +0000

    Linked list example

---
+++ assignments/LinkedLists/LinkedListExample/LinkedList.App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using LinkedList.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TestEmpty();
+        TestCount();
+        TestSum();
+        TestToStringExplicit();
+
+        Console.WriteLine("all tests passed");
+    }
+
+    public static void TestEmpty()
+    {
+        IntegerLinkedList ill = new IntegerLinkedList();
+        Debug.Assert(0 == ill.Count);
+    }
+
+    public static void TestCount()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Debug.Assert(3 == ill.Count);
+    }
+
+    public static void TestSum()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Debug.Assert(21 == ill.Sum);
+    }
+
+    public static void TestToStringExplicit()
+    {
+        var ill = new IntegerLinkedList(5);
+        ill.Append(7);
+        ill.Append(9);
+        Debug.Assert("{5, 7, 9}" == ill.ToString());
+    }
+}
---
+++ assignments/LinkedLists/LinkedListExample/LinkedList.Lib/IntegerNode.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedList.Lib
+{
+    class IntegerNode
+    {
+        int _value;
+        IntegerNode _next;
+
+        internal int Count => _next == null ? 1 : 1 + _next.Count;
+
+        internal int Sum => _next == null ? _value : _value + _next.Sum;
+
+
+        internal IntegerNode(int v)
+        {
+            _value = v;
+            _next = null;
+        }
+
+        internal void Append(int v)
+        {
+            if (_next == null)
+                _next = new IntegerNode(v);
+            else
+                _next.Append(v);
+        }
+
+        public override string ToString()
+        {
+            return _next == null ? _value.ToString() : $"{_value}, {_next}";
+        }
+    }
+}
