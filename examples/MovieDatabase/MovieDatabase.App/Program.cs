4ead35e52b76dd7fa892292c301a3540c03eea89
Chris Harrison <cah@stpaulsschool.org.uk>
2023-05-10 10:01:57 +0100

    Database integration

---
+++ examples/MovieDatabase/MovieDatabase.App/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Tools > NuGet Pacakge Maanager > Console
+// install-package Microsoft.Data.Sqlite 
+
+using Microsoft.Data.Sqlite;
+
+string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Downloads");
+string filename = Path.Combine(path, "movies.db");
+
+using (var connection = new SqliteConnection($"Data Source={filename.ToString()}"))
+{
+    connection.Open();
+    var command = connection.CreateCommand();
+    string director = "James%";
+    command.CommandText =
+    @"  select title, year 
+        from movies 
+        where director like $director
+    ";
+    command.Parameters.AddWithValue("$director", director);
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var title = reader.GetString(0);
+            int year = reader.GetInt32(1);
+
+            Console.WriteLine($"{title} was made in {year}!");
+        }
+    }
+
+}
+
+Console.ReadKey();
